1428249243ded8cfe379096f816ebcd3f4291c11
ZaraQureshi <qzara819@gmail.com>
2026-05-01 12:30:32 +0100

    added methods separation of concerns

--- scripts/train.py
+++ scripts/train.py
@@ -3,7 +3,30 @@ from src.data import DataLoader
 from src.models import PriorityPredictor, SimilarTicketFinder
 from src.storage import HFStorage
 
+def train_model(issues,model_path,token_path):
+    
+    # Train priority model
+    print("Training priority model...")
+    predictor = PriorityPredictor()
+    
+    meta = predictor.training_process(issues, model_path,token_path)
+    print(f"Model trained: {model_path}")
 
+def create_embeddings(settings,issues):
+     # Build similarity index
+    print("Building similarity index...")
+    finder = SimilarTicketFinder()
+    embeddings_path = str(settings.model_dir / "similarity_index.joblib")
+    finder.save_path = embeddings_path
+    finder.compute_and_save_embeddings(issues)
+    print(f"Embeddings saved: {embeddings_path}")
+
+def upload_HF(settings, model_path, embeddings_path):
+    print("Uploading to HF Hub...")
+    storage = HFStorage(settings.hf_repo_id, settings.hf_token)
+    storage.upload_model(model_path, "priority_model_v1/")
+    storage.upload_file(embeddings_path, "embeddings/similarity_index.joblib")
+    print("Upload complete!")
 def main():
     settings=get_settings()
 
@@ -13,28 +36,18 @@ def main():
         str(settings.data_dir / "comments.csv")
     )
     issues = loader.load_issues()
-    # Train priority model
-    print("Training priority model...")
-    predictor = PriorityPredictor()
+    embeddings_path = str(settings.model_dir / "similarity_index.joblib")
     model_path = str(settings.model_dir / "priority_model_v1")
     token_path=str(settings.token_dir)
-    meta = predictor.training_process(issues, model_path,token_path)
-    print(f"Model trained: {model_path}")
+
+    # Train priority model
+    # train_model(issues,model_path,token_path)
 
     # Build similarity index
-    print("Building similarity index...")
-    finder = SimilarTicketFinder()
-    embeddings_path = str(settings.model_dir / "similarity_index.joblib")
-    finder.save_path = embeddings_path
-    finder.compute_and_save_embeddings(issues)
-    print(f"Embeddings saved: {embeddings_path}")
+    # create_embeddings(settings,issues)
     
     # Upload to HF Hub
-    print("Uploading to HF Hub...")
-    storage = HFStorage(settings.hf_repo_id, settings.hf_token)
-    storage.upload_model(model_path, "priority_model_v1/")
-    storage.upload_file(embeddings_path, "embeddings/similarity_index.joblib")
-    print("Upload complete!")
+    upload_HF(settings, model_path, embeddings_path)
 
 if __name__ == "__main__":
     main()
