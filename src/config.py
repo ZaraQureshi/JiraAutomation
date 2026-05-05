0a972fe53a15a80174e663fa776d4f79d647bb0d
ZaraQureshi <qzara819@gmail.com>
2026-05-05 14:39:29 +0100

    modified path for env to get accessed from root

--- src/config.py
+++ src/config.py
@@ -1,21 +1,24 @@
 import os
 from pathlib import Path
 from functools import lru_cache
+from typing import Optional
 from pydantic_settings import BaseSettings
+ROOT_DIR = Path(__file__).resolve().parent.parent
 class Settings(BaseSettings):
-    #Hugging face repo
-    hf_repo_id:str
-    hf_token:str=None
+    # use optional to make these fields optional
+    hf_repo_id: Optional[str] = None
+    hf_token: Optional[str] = None
+    api_url: Optional[str] = None
 
     #Paths
-    base_dir:Path=Path(__file__).parent.parent
+    base_dir:Path=ROOT_DIR
     data_dir:Path=Path("./data")
     token_dir:Path=Path("./artifacts/tokenized_data")
     model_dir:Path=Path("./artifacts/models")
     cache_dir:Path=Path("./artifacts/cache")
 
     class Config:
-        env_file = ".env"
+        env_file = str(ROOT_DIR/".env")
         env_file_encoding = "utf-8"
         case_sensitive = False
 
